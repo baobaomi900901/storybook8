4120ccc6d24e161f8edca23655cf8d9096830328
陆贵顺 <lugs@kingsware.cn>
2024-05-29 21:45:41 +0800

    添加:github工作流

---
+++ storybook-static/assets/Bbm.stories-C5zzfPEC.js
@@ -0,0 +1,5 @@
+import{d as n,o as l,c as m,r as c,b as d,t as p}from"./vue.esm-bundler-7C2mjde0.js";const b={id:"bbm",class:"bbm text-red-500 h-4 w-4"},r=n({__name:"Bbm",props:{label:{default:"bbm1"}},setup(u){return(a,i)=>(l(),m("div",b,[c(a.$slots,"default",{},()=>[d(p(a.label),1)])]))}});r.__docgenInfo={exportName:"default",displayName:"Bbm",description:"",tags:{},props:[{name:"label",required:!0,type:{name:"string"},defaultValue:{func:!1,value:'"bbm1"'}}],slots:[{name:"default"}],sourceFiles:["D:/RengarTang/Kingsware/code_demo/storybook8/src/stories/Bbm/Bbm.vue"]};const f={title:"Example/Bbm",component:r,tags:["autodocs"]},e={args:{label:"Bbm"}};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: 'Bbm'
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const g=["test"];export{g as __namedExportsOrder,f as default,e as test};
