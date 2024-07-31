e0e5b2a9d8e60a60019ace1fb883bac36cc62360
GitHubActions <GitHubActions@github.com>
2024-07-31 07:55:26 +0000

    Deploying to gh-pages from @ baobaomi900901/storybook8@d0bd4efdb003e35cffbb5a9aaa520a6b0c665e89 🚀

--- assets/Configure-BGfAtr_t.js
+++ assets/Configure-BGfAtr_t.js
@@ -1,4 +1,4 @@
-import{ag as p,ap as x}from"./index-DW4YPXil.js";import{u as c}from"./index-CyRhgIVU.js";import"./iframe-BYXUoTIw.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DlJr0YTq.js";import"./index-DrFu-skq.js";var h={exports:{}},a={};/**
+import{ag as p,ap as x}from"./index-CB_ma0hd.js";import{u as c}from"./index-DGn5HzyD.js";import"./iframe-BkW1hwha.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DlJr0YTq.js";import"./index-DrFu-skq.js";var h={exports:{}},a={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
--- assets/KTag.stories-Df01EIAA.js
+++ assets/KTag.stories-Df01EIAA.js
@@ -1,4 +1,4 @@
-import"./button-Co_YL6s_.js";import{c as _}from"./tree_select-D2XSVbIY.js";import"./radio_group-C7MBaVFz.js";import{A as g}from"./index-wXGUTIPA.js";import{d as f,b as x,j as T,y as v,c as k,f as b,o as K,x as E}from"./vue.esm-bundler-COzhbkL3.js";import"./index-nFJc4fXK.js";import"./_commonjsHelpers-Cpj98o6Y.js";const D={class:"flex items-star gap-2"},N={class:"w-80"},y=f({__name:"block",setup(s){return(w,C)=>{const a=x("KTag");return K(),T("div",D,[v("div",N,[k(a,null,{default:b(()=>[E("标签1")]),_:1})])])}}});y.__docgenInfo={exportName:"default",displayName:"block",description:"",tags:{},sourceFiles:["/home/runner/work/storybook8/storybook8/src/DocS/tag/block.vue"]};const S=`<template>
+import"./button-BmBQGL3n.js";import{c as _}from"./tree_select-B-cMQ_oz.js";import"./radio_group-CTOfIhN6.js";import{A as g}from"./index-wXGUTIPA.js";import{d as f,b as x,j as T,y as v,c as k,f as b,o as K,x as E}from"./vue.esm-bundler-zDk3ptr0.js";import"./index-nFJc4fXK.js";import"./_commonjsHelpers-Cpj98o6Y.js";const N={class:"flex items-star gap-2"},D={class:"w-80"},y=f({__name:"block",setup(s){return(w,C)=>{const a=x("KTag");return K(),T("div",N,[v("div",D,[k(a,null,{default:b(()=>[E("标签1")]),_:1})])])}}});y.__docgenInfo={exportName:"default",displayName:"block",description:"",tags:{},sourceFiles:["/home/runner/work/storybook8/storybook8/src/doc/tag/block.vue"]};const A=`<template>
   <div class="flex items-star gap-2">
     <div class="w-80">
       <KTag>标签1</KTag>
@@ -9,7 +9,7 @@ import"./button-Co_YL6s_.js";import{c as _}from"./tree_select-D2XSVbIY.js";impor
 <script lang="ts" setup>
 import { ref } from 'vue';
 <\/script>
-`,A={class:"flex items-star gap-2"},$={class:"w-80"},h=f({__name:"point",setup(s){return(w,C)=>{const a=x("KTag");return K(),T("div",A,[v("div",$,[k(a,{point:""},{default:b(()=>[E("标签1")]),_:1})])])}}});h.__docgenInfo={exportName:"default",displayName:"point",description:"",tags:{},sourceFiles:["/home/runner/work/storybook8/storybook8/src/DocS/tag/point.vue"]};const B=`<template>
+`,$={class:"flex items-star gap-2"},B={class:"w-80"},h=f({__name:"point",setup(s){return(w,C)=>{const a=x("KTag");return K(),T("div",$,[v("div",B,[k(a,{point:""},{default:b(()=>[E("标签1")]),_:1})])])}}});h.__docgenInfo={exportName:"default",displayName:"point",description:"",tags:{},sourceFiles:["/home/runner/work/storybook8/storybook8/src/doc/tag/point.vue"]};const S=`<template>
   <div class="flex items-star gap-2">
     <div class="w-80">
       <KTag point>标签1</KTag>
@@ -21,7 +21,7 @@ import { ref } from 'vue';
 import { ref } from 'vue';
 <\/script>
 `,G={title:"Data Entry(DE)/KTag",component:_,tags:["autodocs"],argTypes:{size:{control:"select",options:["base","sm"]},direction:{control:"select",options:["row","column"]}},args:{size:"base",direction:"row"}},e={args:{},render:s=>({components:{KTag:_},setup(){return{args:s}},template:`
-     <KTag>123</KTag>`})},o=g(y,"块状标签",S),t=g(h,"点状标签",B);var r,n,c;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+     <KTag>123</KTag>`})},o=g(y,"块状标签",A),t=g(h,"点状标签",S);var r,n,c;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
   args: {},
   render: (args: any) => {
     return {
