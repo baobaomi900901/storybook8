29e94d4675f5477f3b0c5e67f04407a601ef0f43
baobaomi900901 <baobaomi900901@icloud.com>
2024-09-02 15:43:53 +0800

    单号: null, 提交人: MT, 内容: 添加树表格示例

--- src/doc/treeTable/CellLayout.vue
+++ src/doc/treeTable/CellLayout.vue
@@ -1,4 +1,17 @@
 <template>
+  <!-- 单元格布局 -->
+  <!-- 通过align属性设置单元格的对齐方式，可选值有left、center、right，默认为left，
+      也可以在列配置中设置align属性，优先级高于align属性
+  -->
+  <SBExamplePanel label="注释" open>
+    <p>
+      单元格布局:
+      <br />
+      通过align属性设置单元格的对齐方式，可选值有left、center、right，默认为left，
+      也可以在列配置中设置align属性，优先级高于align属性
+    </p>
+  </SBExamplePanel>
+  <br />
   <div :style="{ height: '300px' }">
     <k-tree-table
       :data="tableData"
